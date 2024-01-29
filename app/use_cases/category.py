132eb7c3579e750b82b5923ee878c02c025bd0bb
FelipeHardmann <fashardmann@gmail.com>
2024-01-28 22:44:36 -0300

    :sparkles: feat: add feature para delete

--- app/use_cases/category.py
+++ app/use_cases/category.py
@@ -1,6 +1,8 @@
 from app.db.models import Category as CategoryModel
 from app.schemas.category import Category, CategoryOutput
 from sqlalchemy.orm import Session
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
 
 class CategoryUseCases:
@@ -19,6 +21,18 @@ class CategoryUseCases:
             for category_model in categories_on_db
         ]
         return categories_output
+
+    def delete_category(self, id: int):
+        category_model = self.db_session.query(
+            CategoryModel
+        ).filter_by(id=id).first()
+        if not category_model:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Category Not found'
+            )
+        self.db_session.delete(category_model)
+        self.db_session.commit()
 
     def serialize_category(self, category_model: CategoryModel):
         return CategoryOutput(**category_model.__dict__)

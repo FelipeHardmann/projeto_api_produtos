6f155282a07c4e31fd07c18b6c01b8799d166dd0
FelipeHardmann <fashardmann@gmail.com>
2024-02-05 23:46:07 -0300

    :sparkles: feat: add new models

--- app/db/models.py
+++ app/db/models.py
@@ -22,8 +22,13 @@ from app.db.base import Base
 from sqlalchemy import (
     Column,
     Integer,
-    String
+    String,
+    ForeignKey,
+    DateTime,
+    Float,
+    func
 )
+from sqlalchemy.orm import relationship
 
 
 class Category(Base):
@@ -31,3 +36,19 @@ class Category(Base):
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String, nullable=False)
     slug = Column('slug', String, nullable=False)
+    products = relationship('Product', back_populates='category')
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String, nullable=False)
+    slug = Column('slug', String, nullable=False)
+    price = Column('price', Float, nullable=False)
+    stock = Column('stock', Integer)
+    created_at = Column('created_at', DateTime, server_default=func.now())
+    updated_at = Column('updated_at', DateTime, onupdate=func.now())
+    category_id = Column(
+        'category_id', ForeignKey('categories.id'), nullable=False
+    )
+    category = relationship('Category', back_populates='products')

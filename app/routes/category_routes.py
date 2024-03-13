bcc48bd17035cb5aa4a0b65db9935aea8acdf970
FelipeHardmann <fashardmann@gmail.com>
2024-03-12 23:36:03 -0300

    :books: docs: refactore docs routers

--- app/routes/category_routes.py
+++ app/routes/category_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, Response, status
-from app.schemas.category import Category
+from typing import List
+from app.schemas.category import Category, CategoryOutput
 from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.use_cases.category import CategoryUseCases
@@ -8,7 +9,11 @@ from app.use_cases.category import CategoryUseCases
 router = APIRouter(prefix='/category', tags=['Category'])
 
 
-@router.post('/add')
+@router.post(
+        '/add',
+        status_code=status.HTTP_201_CREATED,
+        description='Add new Category'
+    )
 def add_category(
     category: Category,
     db_session: Session = Depends(get_db_session)
@@ -18,7 +23,11 @@ def add_category(
     return Response(status_code=status.HTTP_201_CREATED)
 
 
-@router.get('/list')
+@router.get(
+        '/list',
+        response_model=List[CategoryOutput],
+        description='List Category'
+    )
 def list_category(
     db_session: Session = Depends(get_db_session)
 ):
@@ -28,7 +37,10 @@ def list_category(
     return response
 
 
-@router.delete('/delete/{id}')
+@router.delete(
+        '/delete/{id}',
+        description='Delete Category'
+    )
 def delete_category(
     id: int,
     db_session: Session = Depends(get_db_session)
